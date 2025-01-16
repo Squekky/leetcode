08895623900908dd51e1899f693789339742616f
Squekky <squekkyy@gmail.com>
2025-01-16 14:46:30 -0500

    modified Container With Most Water to improve time complexity

--- medium/11-container-with-most-water/solution.cpp
+++ medium/11-container-with-most-water/solution.cpp
@@ -1,7 +1,6 @@
 #include <vector>
 using namespace std;
 
-// Struggles with large vectors
 
 class Solution {
 public:
@@ -11,13 +10,18 @@ public:
             return min(height[0], height[1]);
         }
 
-        for (int i = 0; i < height.size() - 1; i++) {
-            for (int j = i + 1; j < height.size(); j++) {
-                // left bar = height[i]
-                // right bar = height[j]
-                // width = j - i
-                int water_contained = (j - i) * min(height[i], height[j]);
-                if (water_contained > max_water) max_water = water_contained;
+        int left = 0;
+        int right = height.size() - 1;
+
+        while (left < right) {
+            int width = right - left;
+            int water_contained = width * min(height[left], height[right]);
+            if (water_contained > max_water) max_water = water_contained;
+            
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
             }
         }
         return max_water;
